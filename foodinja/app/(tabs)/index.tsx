763cff027231c752aef7645463570110c0b79fea
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2026-02-11 13:20:32 +0330

    add font and test

---
+++ foodinja/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="font-vazir text-2xl text-red-500">
+        سلام به فوداینجا خوش اومدی
+      </Text>
+    </View>
+  );
+}
--- foodinja/app/_layout.tsx
+++ foodinja/app/_layout.tsx
@@ -1,24 +1,36 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import "../global.css"
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import 'react-native-reanimated'
-import 'react-native-gesture-handler'
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useFonts } from "expo-font";
+import { ActivityIndicator, View } from "react-native";
+import "react-native-reanimated";
+import "react-native-gesture-handler";
+import "../global.css";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  const [fontsLoaded] = useFonts({
+    VazirMedium: require("../assets/font/Vazir-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
