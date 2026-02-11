b108c49e323fb113fed71dc6b1eeb2805b061ab2
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2026-02-12 00:42:49 +0330

    update

--- foodinja/components/DrawerContent.tsx
+++ foodinja/components/DrawerContent.tsx
@@ -5,7 +5,6 @@ export default function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
       <Text style={{ fontSize: 18, margin: 16 }}>Chats</Text>
-      {/* اینجا لیست conversations */}
       
       <Text style={{ fontSize: 18, margin: 16 }}>Other</Text>
     </DrawerContentScrollView>
